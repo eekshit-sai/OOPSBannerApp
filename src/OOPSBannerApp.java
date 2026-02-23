8638f49fe06c71e68333010f0ea175fa8c26c7af
eekshit-sai <eekshitsaigonela@gmail.com>
2026-02-23 13:40:34 +0530

    UC2

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,17 +1,18 @@
-/**
- * OOPSBannerApp UC1 - OOPS Banner Display Application
- *
- * This class demonstrates a simple Java application that displays the Object
- * Oriented Programming System OOPS acronym to the console.
- *
- * @author eekshit
- * @version 1.0
- */
+/*
+*OOPSBannerApp UC2
+*
+*@author 
+*@version 2.0 
+*/
 
-public class OOPSBannerApp {
-
+public class OOPSBannerApp{
     public static void main(String[] args) {
-        System.out.println("OOPS");
+        System.out.println("   *** " + " " + "     *** " + " " + "   ****** " + " " + "       ***** ");
+        System.out.println(" **   ** " + " " + " **   ** " + " " + " **    ** " + " " + "   **     ** ");
+        System.out.println(" **   ** " + " " + " **   ** " + " " + " **    ** " + " " + " ** ");
+        System.out.println(" **   ** " + " " + " **   ** " + " " + " ****** " + " " + "      ***** ");
+        System.out.println(" **   ** " + " " + " **   ** " + " " + " ** " + " " + "               ** ");
+        System.out.println(" **   ** " + " " + " **   ** " + " " + " ** " + " " + "     **      ** ");
+        System.out.println("   *** " + " " + "     *** " + " " + "   ** " + " " + "        ***** ");
     }
-
 }
